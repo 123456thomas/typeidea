3b819ff2b78ab8f0b11eeedede46f730d9cc8dcc
123456thomas <17625809083@sina.cn>
2020-02-04 11:41:44 +0800

    给blog/model.py里的模型增加__str__方法；并初步配置admin页面

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -20,6 +20,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '分类'
 
+    def __str__(self):
+        return self.name
+
 
 # 创建标签模型
 class Tag(models.Model):
@@ -37,6 +40,9 @@ class Tag(models.Model):
 
     class Meta:
         verbose_name = verbose_name_plural = '标签'
+
+    def __str__(self):
+        return self.name
 
 
 #创建文章模型
@@ -63,4 +69,6 @@ class Post(models.Model):
         verbose_name = verbose_name_plural = '文章'
         ordering = ['-id']  # 根据id进行降序排列
 
-8
+
+    def __str__(self):
+        return self.title
